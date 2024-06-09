2eb6cc2df2d22f5b8fac9a63f7e1af53112ddcb8
Verona0609 <dikaya.nika15@gmail.com>
2024-06-09 10:09:21 +0200

    hw 12

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ if (page * 15 >= data.totalHits){
 toggleLoadMoreButton(false);
 iziToast.info({
   title:"Error",
-  message: "We're sorry, but you've reached the end of search results.",
+  message: "We're sorry, but you've reached the end of search results.", 
   position: "topRight"
 })
 }else{
