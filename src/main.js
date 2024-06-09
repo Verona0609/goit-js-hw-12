01ea164e92d732e9da37c66656ab2e2b181a4be3
Verona0609 <dikaya.nika15@gmail.com>
2024-06-09 09:50:16 +0200

    hw12

--- src/main.js
+++ src/main.js
@@ -17,23 +17,6 @@ const loadMoreBtn = document.querySelector(".load-more")
 
 
 
-formEl.addEventListener("submit", async(e) =>{
-  e.preventDefault();
-
-  query = e.target.elements.query.value.trim();
-
-  if (!query){
-    showError("Please enter a search query!");
-    return;
-  }
-
-  page = 1;
-  clearGallery();
-   await fetchImages();
-});
-
-
-
 
 
 const fetchImages = async() => {
@@ -42,18 +25,12 @@ const fetchImages = async() => {
     const data = await searchImages(query, page);
     hideLoading();
 
-    if( data.hits.lenght === 0 && pade === 1){
+    if( data.hits.lenght === 0 && page === 1){
       showError ( "Sorry, there are no images matching your search query. Please try again!");
       return;
     }
 renderImages(data.hits);
 page += 1;
-
-
-loadMoreBtn.addEventListener("click", fetchImages);
-
-
-
 
 
 
@@ -75,3 +52,22 @@ console.log(error);
 }
 
 };
+
+formEl.addEventListener("submit", async(e) =>{
+  e.preventDefault();
+
+  query = e.target.elements.query.value.trim();
+
+  if (!query){
+    showError("Please enter a search query!");
+    return;
+  }
+
+  page = 1;
+  clearGallery();
+   await fetchImages();
+});
+
+
+
+loadMoreBtn.addEventListener("click", fetchImages);
